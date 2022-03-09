7b3fb964cd3004fd957fd768aa25281c806f4926
Bora Arslan <bora-arslan@outlook.com>
2022-03-09 20:30:46 +0300

    Added update character and db changes

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,36 @@ pub mod schema;
 
 use diesel::prelude::*;
 use dotenv::dotenv;
-use std::env;
+use std::{env, fmt::{Display, Debug}};
 
 use models::*;
 use schema::*;
 
+
+#[derive(Debug, poise::ChoiceParameter, Clone, Copy)]
+pub enum Class {
+    Berserker,
+    Paladin,
+    Gunlancer,
+    Striker,
+    Wardancer,
+    Scrapper,
+    Soulfist,
+    Gunslinger,
+    Artillerist,
+    Deadeye,
+    Sharpshooter,
+    Bard,
+    Sorceress,
+    Shadowhunter,
+    Deathblade,
+}
+
+impl Display for Class {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self, f)
+    }
+}
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
@@ -18,11 +43,11 @@ pub fn establish_connection() -> SqliteConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-pub fn insert_server(server_id: i32, server_name: &str) -> Result<(), diesel::result::Error> {
+pub fn insert_server(server_id: u64, server_name: &str) -> Result<(), diesel::result::Error> {
     let connection = establish_connection();
 
     let new_server = Server {
-        id: server_id,
+        id: server_id.to_string(),
         guild_name: server_name.to_string(),
     };
 
@@ -34,12 +59,12 @@ pub fn insert_server(server_id: i32, server_name: &str) -> Result<(), diesel::re
     Ok(())
 }
 
-pub fn insert_guildmate(server_id: i32, guildmate_id: i32) -> Result<(), diesel::result::Error> {
+pub fn insert_guildmate(server_id: u64, guildmate_id: u64) -> Result<(), diesel::result::Error> {
     let connection = establish_connection();
 
     let new_guildmate = Guildmate {
-        id: guildmate_id,
-        server_id: server_id,
+        id: guildmate_id.to_string(),
+        server_id: server_id.to_string(),
     };
 
     diesel::insert_into(guildmates::table)
@@ -51,15 +76,15 @@ pub fn insert_guildmate(server_id: i32, guildmate_id: i32) -> Result<(), diesel:
 }
 
 pub fn insert_character(
-    character_id: i32,
+    character_id: u64,
     character_name: &str,
-    character_class: &str,
+    character_class: Class,
     character_item_level: i32,
 ) -> Result<(), diesel::result::Error> {
     let connection = establish_connection();
 
     let new_character = Character {
-        id: character_id,
+        id: character_id.to_string(),
         name: character_name.to_string(),
         class: character_class.to_string(),
         item_level: character_item_level,
@@ -73,33 +98,33 @@ pub fn insert_character(
     Ok(())
 }
 
-pub fn get_server(server_id: i32) -> Option<Server> {
+pub fn get_server(server_id: u64) -> Option<Server> {
     let connection = establish_connection();
 
     let server = servers::table
-        .filter(servers::id.eq(server_id))
+        .filter(servers::id.eq(server_id.to_string()))
         .get_result(&connection)
         .ok();
 
     server
 }
 
-pub fn get_guildmate(guildmate_id: i32) -> Option<Guildmate> {
+pub fn get_guildmate(guildmate_id: u64) -> Option<Guildmate> {
     let connection = establish_connection();
 
     let guildmate = guildmates::table
-        .filter(guildmates::id.eq(guildmate_id))
+        .filter(guildmates::id.eq(guildmate_id.to_string()))
         .get_result(&connection)
         .ok();
 
     guildmate
 }
 
-pub fn get_guildmate_characters(guildmate_id: i32) -> Option<Vec<Character>> {
+pub fn get_all_characters(guildmate_id: u64) -> Option<Vec<Character>> {
     let connection = establish_connection();
 
     let characters = characters::table
-        .filter(characters::id.eq(guildmate_id))
+        .filter(characters::id.eq(guildmate_id.to_string()))
         .get_results(&connection)
         .ok();
 
@@ -117,11 +142,34 @@ pub fn get_single_character(character_name: &str) -> Option<Character> {
     character
 }
 
-// This also deletes all the guildmates and characters associated with the server
-pub fn remove_server(server_id: i32) -> Result<(), diesel::result::Error> {
+pub fn update_character(
+    character_name: &str,
+    character_class: Class,
+    character_item_level: i32,
+) -> Result<(), diesel::result::Error> {
     let connection = establish_connection();
 
-    diesel::delete(servers::table.filter(servers::id.eq(server_id)))
+    let existing_character = get_single_character(character_name);
+    if let None = existing_character {
+        return Err(diesel::result::Error::NotFound);
+    }
+    let mut existing_character = existing_character.unwrap();
+    existing_character.item_level = character_item_level;
+    existing_character.class = character_class.to_string();
+
+    diesel::replace_into(characters::table)
+        .values(existing_character)
+        .execute(&connection)
+        .expect("Error updating character");
+
+    Ok(())
+}
+
+// This also deletes all the guildmates and characters associated with the server
+pub fn remove_server(server_id: u64) -> Result<(), diesel::result::Error> {
+    let connection = establish_connection();
+
+    diesel::delete(servers::table.filter(servers::id.eq(server_id.to_string())))
         .execute(&connection)
         .expect("Error deleting server");
 
@@ -129,10 +177,10 @@ pub fn remove_server(server_id: i32) -> Result<(), diesel::result::Error> {
 }
 
 // This also deletes all characters associated with the guildmate
-pub fn remove_guildmate(guildmate_id: i32) -> Result<(), diesel::result::Error> {
+pub fn remove_guildmate(guildmate_id: u64) -> Result<(), diesel::result::Error> {
     let connection = establish_connection();
 
-    diesel::delete(guildmates::table.filter(guildmates::id.eq(guildmate_id)))
+    diesel::delete(guildmates::table.filter(guildmates::id.eq(guildmate_id.to_string())))
         .execute(&connection)
         .expect("Error deleting guildmate");
 
