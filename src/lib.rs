5c11fc574c5aaaebdabe42062e39c6f1d27fce09
Bora Arslan <bora-arslan@outlook.com>
2022-03-15 04:36:44 +0300

    Moved Class enum to entity crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,4 @@ pub mod database;
 
 use std::fmt::{Debug, Display};
 use entity::*;
-
-
-#[derive(Debug, poise::ChoiceParameter, Clone, Copy)]
-pub enum Class {
-    Berserker,
-    Paladin,
-    Gunlancer,
-    Striker,
-    Wardancer,
-    Scrapper,
-    Soulfist,
-    Gunslinger,
-    Artillerist,
-    Deadeye,
-    Sharpshooter,
-    Bard,
-    Sorceress,
-    Shadowhunter,
-    Deathblade,
-}
-
-impl Display for Class {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(&self, f)
-    }
-}
+pub use entity::characters::Class;
